783f110f3b9a5ab4a4280600e85250ce83553c7c
Thomas Constantine Moore <tomismore@gmail.com>
2024-09-01 18:22:44 -0400

    rename path to assets since the public dir is not automatically deployed at the root

--- app/webpack.config.ts
+++ app/webpack.config.ts
@@ -28,7 +28,7 @@ const config: webpack.Configuration = {
     new CopyWebpackPlugin({
       patterns: [
         { from: "index.html", to: "index.html" },
-        { from: "public", to: "public"}
+        { from: "assets", to: "assets"}
       ],
     })
   ],
